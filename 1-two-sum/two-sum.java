885b3e25160eab4c761a443a75a488c190594ef4
Atchaya K A <ap.atchaya436@gmail.com>
2026-08-19 21:01:40 +0530

    Time: 45 ms (27.00%) | Memory: 46.8 MB (91.01%) - LeetSync

---
+++ 1-two-sum/two-sum.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.util.Arrays;
+class Solution 
+{
+    public int[] twoSum(int[] nums, int target) 
+    {
+        for (int i=0;i<nums.length;i++)
+        {
+            for(int j=i+1;j<nums.length;j++)
+            {
+                if (nums[i] + nums[j] == target)
+                {
+                    return new int[]{i,j};
+                }
+            }
+        }
+        return new int[]{};
+    }
+    public static void main(String[] args)
+    {
+        Solution s = new Solution();
+        int nums[]={2,7,11,15};
+        int target=9;
+        System.out.println(Arrays.toString(s.twoSum(nums,target)));
+    }
+}
